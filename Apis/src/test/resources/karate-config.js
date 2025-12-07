9d2403f204dd27fb646150c3313dcb66ace0e2e5
aabernal <aabernal@bancolombia.com.co>
2025-12-07 13:54:52 -0500

    feat: Integrate API tests into CI pipeline

--- Apis/src/test/resources/karate-config.js
+++ Apis/src/test/resources/karate-config.js
@@ -1,7 +1,5 @@
 function fn() {
-  // Karate configuration file.  You can override the baseUrl by passing
-  // -DbaseUrl=http://yourhost on the Maven command line.  If not
-  // provided, the OrangeHRM demo site is used by default.
+
   var config = {
     baseUrl: karate.properties['baseUrl'] || 'https://opensource-demo.orangehrmlive.com'
   };
